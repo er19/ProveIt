5e53893d02e357cdb2993a1a0d1e675641a1fa10
er19 <evanrichard1911@gmail.com>
2025-07-10 00:35:36 +0200

    fix exercise 2 button on reformulate equation and outro comments

--- ProveItApp/app/pages/mathematical-induction/exercise-2/outro.vue
+++ ProveItApp/app/pages/mathematical-induction/exercise-2/outro.vue
@@ -13,13 +13,11 @@ const params = useRoute().params
             <!-- Speech Bubble -->
             <div class="bg-white border-4 border-black rounded-xl p-6 text-left inline-block relative text-lg font-medium mx-auto">
                 <p class="">
-                    Exercise 1, done! Good job! <br/>
-                    You have proven the definition of odd number by <br/>
-                    mathematical induction. This will be quiet helpful <br/>
-                    and you can use the statement in other proof to <br/>
-                    support your argument, especially in induction step.<br/>
-                    Your progress bar also show progress. Keep going!<br/>
-                    In no time, you will be independent
+                    Exercise 2, done! Good job! <br/>
+                    You have proven a statement with little help from me. <br/>
+                    Now your progress bar has shown that you are ready to write proof by yourself.<br/>
+                    Congrats! If you have completed exercise 1 and exercise 2,<br/>
+                    the final exercise will be opened. Have fun with it !
                 </p>
                 <!-- Speech bubble tail -->
                 <div class="absolute bottom-[-20px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-r-[10px] border-t-[20px] border-l-transparent border-r-transparent border-t-black"></div>
